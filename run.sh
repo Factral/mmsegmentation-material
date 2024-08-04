d9ac9ccbe544a497b92e8fc08a8e4808f17d397a
Fabian *-* <nelsonfabiancs8@gmail.com>
2024-08-04 11:16:55 -0500

    fix and set gpu

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-python tools/train.py --config local_configs/segformer_mit-b0_8xb2-160k_hsi-512x512.py \
-    --amp --launcher pytorch
+CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+    --config local_configs/segformer_mit-b0_8xb2-160k_hsi-512x512.py \
+    --amp
